5e08630eaec98096bca825699e57a4352edf2403
Mykola Diachok <ndyachok@gmail.com>
2021-09-27 23:43:56 +0300

    half work variant

--- src/accessory.ts
+++ src/accessory.ts
@@ -3,14 +3,18 @@ import {
     AccessoryPlugin,
     API,
     Logging,
-    Service
+    Service,
+    CharacteristicValue
 } from "homebridge";
 import { MANU_FACTURER, MODEL } from './settings';
+import superagent from "superagent";
+
 
 
 
 export interface TerneoHeatFloorAccessoryConfig extends AccessoryConfig {
-    ipAddress: string;
+    host: string;
+    serial: string;
 }
 
 export class ExampleSwitch implements AccessoryPlugin {
@@ -21,7 +25,7 @@ export class ExampleSwitch implements AccessoryPlugin {
     private api: API;
 
     private informationService: Service;
-    public readonly switchService: any
+    private switchService: Service;
 
     constructor(log: Logging, config: AccessoryConfig, api: API) {
         this.log = log;
@@ -29,28 +33,72 @@ export class ExampleSwitch implements AccessoryPlugin {
         this.config = config as TerneoHeatFloorAccessoryConfig;
         this.api = api;
 
+
+
         this.informationService = new api.hap.Service.AccessoryInformation()
             .setCharacteristic(api.hap.Characteristic.Manufacturer, MANU_FACTURER)
-            .setCharacteristic(api.hap.Characteristic.Model, MODEL);
+            .setCharacteristic(api.hap.Characteristic.Model, MODEL)
+            .setCharacteristic(api.hap.Characteristic.SerialNumber, this.config.serial);
 
-        this.switchService = new this.api.hap.Service.Switch(this.config.name)
 
+        this.switchService = new this.api.hap.Service.Switch(this.config.name);
         this.switchService.getCharacteristic(this.api.hap.Characteristic.On)
-            .on('get', this.getOnHandler.bind(this))
-            .on('set', this.setOnHandler.bind(this))
+            .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
+            .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
 
         log.info("Switch finished initializing!");
+        log.info("host:" + this.config.host);
+        log.info("serial:" + this.config.serial);
+        this.status = false;
     }
 
-    getOnHandler(callback: any) {
-        const on = true
-        this.log.info('Getting switch state: ', on)
-        callback(null, on)
+    private status: boolean;
+
+    setOn(value: CharacteristicValue) {
+        // implement your own code to turn your device on/off
+        // this.exampleStates.On = value as boolean;
+        this.status = value as boolean;
+        let url = 'http://' + this.config.host + '/api.cgi';
+        this.log.debug('http->', url);
+        const prepareBody = {
+            sn: this.config.serial,
+            par: [[125, 7, Number(!value).toString()]]
+        };
+        this.log.debug('prepare body->', prepareBody);
+
+        superagent
+            .post(url)
+            .send(prepareBody)
+            .timeout(5000)
+            .then((res) => {
+                this.log.debug('response:', res.body)
+                this.log.debug('status:', res.status)
+                if (res.status === 200) {
+                    var parms: Array<Array<{ param: number, type: number, value: string }>> = [[125, 7, "0"], [5, 1, "27"]];
+                }
+            })
+            .catch((error) => {
+                this.log.error('Error:', error.message)
+            });
+
+        // const response = await fetch(this.config.host + '/api.cgi', {
+        //     method: 'get',
+        //     body: body,
+        //     headers: { 'Content-Type': 'application/json' }
+        // });
+        // const data = await response.json();
+        //this.log.debug('response:', data);
+        this.log.debug('Set Characteristic On ->', this.status);
     }
 
-    setOnHandler(value: boolean, callback: any) {
-        this.log.info('Setting switch state to: ', value)
-        callback(null)
+
+    getOn() {
+        // implement your own code to check if the device is on
+        // const isOn = this.exampleStates.On;
+
+        this.log.debug('Get Characteristic On ->', this.status);
+
+        return this.status;
     }
 
     /*
@@ -66,10 +114,7 @@ export class ExampleSwitch implements AccessoryPlugin {
      * It should return all services which should be added to the accessory.
      */
     getServices(): Service[] {
-        return [
-            this.informationService,
-            this.switchService,
-        ];
+        return [this.switchService, this.informationService];
     }
 
 }
